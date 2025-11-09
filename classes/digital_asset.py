f10a6f5ac5e19b282778bf2d0fa85792d4f9208e
mendy hershkop <hers.mm@gmail.com>
2025-11-09 16:52:38 +0200

    digital asset classes

---
+++ classes/digital_asset.py
@@ -0,0 +1,24 @@
+import datetime
+from abc import ABC,abstractmethod
+
+class DigitalAsset(ABC):
+    def __init__(self,name, cost:float):
+        self.__name=name
+        self.__registration_data=datetime.date.today()
+        self.__cost=cost
+
+    @property
+    def name(self):
+        return self.__name
+    @property
+    def time(self):
+        return self.__registration_data.isoformat()
+    @property
+    def cost(self):
+        return self.__cost
+    @abstractmethod
+    def calculate_value(self):
+        pass
+    @abstractmethod
+    def asset_type(self):
+        pass
