70e4413f1b50c4b5b4eb5e4917137e431fedde83
mendy hershkop <hers.mm@gmail.com>
2025-11-10 09:22:33 +0200

    ended create assert portfolio

--- classes/webside.py
+++ classes/webside.py
@@ -13,4 +13,4 @@ class Webside(DigitalAsset, Reportable):
         value=self._monthly_traffic*self._monetization_rate*12
         return value
     def to_report_line(self):
-        return "name:",self.name,"type:",self.asset_type,"date:",self.time,"cost:",self.cost,"traffic:", self._monthly_traffic,"monetization: ",self._monetization_rate,"value: ", self.calculate_value()
+        return "type:",self.asset_type,"name:",self.name,"date:",self.time,"cost:",self.cost,"traffic:", self._monthly_traffic,"monetization: ",self._monetization_rate,"value: ", self.calculate_value()
--- composition O-i/asset_protfolio.py
+++ composition O-i/asset_protfolio.py
@@ -1,4 +1,6 @@
 from classes.digital_asset import DigitalAsset
+from classes.mobile_app import MobileApp
+from classes.webside import Webside
 
 
 class AssertPortfolio:
@@ -14,14 +16,20 @@ class AssertPortfolio:
         return round(value, 2)
     def save_portfolio(self):
         with open(self.__filename,"w") as file:
-            for i in self.__assets
+            for i in self.__assets:
                 file.write(i.to_reprt_line())
     def load_portfolio(self):
         self.__assets=[]
         try:
-            with open(self.__filename,"w") as file:
+            with open(self.__filename,"r") as file:
                 file.readline()
                 for i in file:
                     ",".join(i)
+                    if i[1] == "WEBSITE":
+                        web=Webside(i[3],float(i[7]),int(i[9]),float(i[11]))
+                        self.add_asset(web)
+                    if i[1] =="MOBILE_APP":
+                        mob=MobileApp(i[3],float(i[7]),int(i[9]),float(i[11]))
+                        self.add_asset(mob)
         except:
             raise FileNotFoundError
